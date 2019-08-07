55db137c074d24eb4a0e3e34fadf33d026c982c2
ImsirovicAjdin <ajdika@live.com>
2019-08-07 12:02:02 +0200

    Task to complete: Define an Observable that emits three strings and subscribe with method for next(), error(), and complete()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,11 +10,19 @@ export class AppComponent implements OnInit {
   name = 'Angular';
 
   ngOnInit() {
+    /*
     of(2,4,6,8).subscribe(console.log);
 
     from([20, 15, 10, 5]).subscribe(
       item => console.log(`resulting item .. ${item}`),
       err => console.log(`error occured .. ${err}`),
+      () => console.log('complete')
+    )
+    */
+    
+    of('Pineapple', 'Apricot', 'Dragonfruit').subscribe(
+      item => console.warn(`${item}`),
+      err => console.info(`${err}`),
       () => console.log('complete')
     )
   }
